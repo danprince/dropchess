54cd897e703ae93fe2adf254898a280ab321957b
Dan Prince <1266011+danprince@users.noreply.github.com>
2024-12-22 16:46:19 -0700

    adds single-player frontend

---
+++ app.js
@@ -0,0 +1,79 @@
+import { Game, white, black } from "./chess.js";
+
+/**
+ * @import { Tile, Piece, Color, Move } from "./chess.js";
+ *
+ * @typedef {object} Square
+ * @prop {Tile} tile
+ * @prop {Color} color
+ * @prop {boolean} selected
+ * @prop {Piece | undefined} piece
+ * @prop {Move | undefined} [move]
+ *
+ * @typedef {object} Room
+ * @prop {Color[]} players
+ * @prop {Color} turn
+ */
+
+class App {
+  /**
+   * @type {Game}
+   */
+  game = new Game();
+
+  /**
+   * The piece that is currently selected.
+   * @type {Piece | undefined}
+   */
+  selectedPiece;
+
+  init() {
+    this.game = new Game();
+  }
+
+  /**
+   * Select a piece.
+   * @param {Piece | undefined} piece
+   */
+  selectPiece(piece) {
+    this.selectedPiece = piece;
+  }
+
+  /**
+   * @param {Move} move
+   */
+  play(move) {
+    if (this.selectedPiece) {
+      this.game.play(this.selectedPiece, move);
+      this.selectPiece(undefined);
+    }
+  }
+
+  /**
+   * @returns {Square[]}
+   */
+  getSquares() {
+    let moves = this.selectedPiece
+      ? this.game.getMoves(this.selectedPiece)
+      : [];
+
+    return this.game.tiles.map((tile) => {
+      let piece = this.game.getPiece(tile);
+
+      let move = moves.find(
+        (move) => move.x === tile.x && move.y === tile.y,
+      );
+
+      let selected =
+        tile.x === this.selectedPiece?.x && tile.y === this.selectedPiece.y;
+
+      let checker = tile.x % 2 ? tile.y % 2 : (tile.y + 1) % 2;
+      /** @type {Color} */
+      let color = checker ? white : black;
+
+      return { tile, color, move, selected, piece };
+    });
+  }
+}
+
+Object.assign(window, { App });
